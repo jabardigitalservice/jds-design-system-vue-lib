586aa323bed3e38ff89c894f7c681cfa85493ab3
Adzhar Amrullah <adzharamrullah@gmail.com>
2021-06-07 09:50:02 +0700

    add and config sass-loader

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  configureWebpack:{
+    module: {
+      rules: [
+        {
+          test: /\.sass$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                indentedSyntax: true,
+                // sass-loader version >= 8
+                sassOptions: {
+                  indentedSyntax: true
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
+  }
+}
