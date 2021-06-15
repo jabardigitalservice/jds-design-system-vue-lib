a4f03113a0ff1cc1a2929071fd6782c9f500ff9a
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-15 10:56:09 +0700

    set vue app devserver

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
+const path = require('path')
+
 module.exports = {
+  devServer: {
+    contentBase: [
+      path.join(__dirname, 'playgrounds/vue/public')
+    ],
+  },
+  pages: {
+    index: {
+      entry: 'playgrounds/vue/main.js',
+      template: 'playgrounds/vue/public/index.html',
+      filename: 'index.html',
+      title: 'Jabar Design System Vue Playground',
+    },
+  },
+  outputDir: 'dist',
   configureWebpack:{
+    resolve: {
+      alias: {
+        '@jabardigitalservice/jds-design-system': path.resolve(__dirname, 'publish')
+      },
+    },
     module: {
       rules: [
         {
