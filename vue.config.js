c94b52711661fe0b7607a42b611595e3cfa70548
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-17 03:57:29 +0700

    use purgecss and resources loader to inject global scss

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,59 @@
 const path = require('path')
 
+function mergeAppWebpackConfig(config) {
+  config.resolve.alias = config.resolve.alias || {}
+  Object.assign(config.resolve.alias, {
+    '@jabardigitalservice/jds-design-system': path.resolve(__dirname, 'publish')
+  })
+}
+
+function mergeWebComponentWebpackConfig(config) {
+  const styleRules = config.module.rules.filter((rule) => {
+    return rule.test.test('.scss')
+  })
+
+  const resourcesLoader = {
+    loader: 'sass-resources-loader',
+    options: {
+      resources: [
+        'styles/scss/index.scss',
+      ],
+    }
+  }
+  const purgeCSSLoader = {
+    loader: '@fullhuman/purgecss-loader',
+    options: {
+      content: [
+        path.join(__dirname, 'src/components/**/*.vue')
+      ],
+    }
+  }
+  styleRules.forEach((rule) => {
+    if (!Array.isArray(rule.oneOf)) {
+      return
+    }
+
+    
+    rule.oneOf
+      .filter((item) => {
+        return item.resourceQuery?.test('module')
+          || item.resourceQuery?.test('?vue')
+      })
+      .forEach((item) => {
+        // set purgeCSSLoader right before css loader
+        const cssLoaderIndex = item.use.findIndex((loader) => {
+          return loader.loader.includes('node_modules/css-loader/')
+        })
+        if (cssLoaderIndex >= 0) {
+          item.use.splice(cssLoaderIndex + 1, 0, purgeCSSLoader)
+        }
+
+        // set resourcesLoader as first loader
+        item.use.push(resourcesLoader)
+      })
+  })
+}
+
 module.exports = {
   devServer: {
     contentBase: [
@@ -16,9 +70,11 @@ module.exports = {
   },
   outputDir: 'dist',
   configureWebpack: (config) => {
-    config.resolve.alias = config.resolve.alias || {}
-    Object.assign(config.resolve.alias, {
-      '@jabardigitalservice/jds-design-system': path.resolve(__dirname, 'publish')
-    })
+    const { VUE_CLI_SERVICE_BUILD_TARGET: target } = process.env
+    if (target === 'app') {
+      mergeAppWebpackConfig(config)
+    } else if (target === 'wc') {
+      mergeWebComponentWebpackConfig(config)
+    }
   },
 }
