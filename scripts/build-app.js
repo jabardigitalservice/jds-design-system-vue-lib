6467175d08158e0d2bbd19c5bf4f68d8bc2d84dc
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-17 03:58:09 +0700

    set env to determine which webpack config to use

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -1,6 +1,7 @@
 const shell = require('shelljs')
 
 shell.exec(`
+  export VUE_CLI_SERVICE_BUILD_TARGET=app && \
   npx vue-cli-service build \
   --mode production \
   --target app \
--- scripts/build-wc.js
+++ scripts/build-wc.js
@@ -3,6 +3,7 @@ const shell = require('shelljs')
 const args = process.argv.slice(2)
 const watch = args.includes('--watch')
 shell.exec(`
+  export VUE_CLI_SERVICE_BUILD_TARGET=wc && \
   npx vue-cli-service build \
   --mode production \
   --target wc \
