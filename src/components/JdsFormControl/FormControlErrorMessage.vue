c5085fb425eae6535b76a2a3cc5cfa75d24fa5b4
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-18 05:05:06 +0700

    add generic component (label, errMsg, helperText)

--- src/components/JdsFormControl/FormControlErrorMessage.vue
+++ src/components/JdsFormControl/FormControlErrorMessage.vue
@@ -3,3 +3,15 @@
     <slot></slot>
   </label>  
 </template>
+
+<style lang="scss">
+@use "~/styles/scss/variables" as V;
+
+.jds-form-control-error-message {
+  margin: 0;
+  padding: 0;
+  font-size: 13px;
+  line-height: 23px;
+  color: V.$red-700;
+}
+</style>
--- src/components/JdsFormControl/FormControlHelperText.vue
+++ src/components/JdsFormControl/FormControlHelperText.vue
@@ -3,3 +3,18 @@
     <slot></slot>
   </label>  
 </template>
+
+<style lang="scss">
+@use "~/styles/scss/variables" as V;
+
+.jds-form-control-helper-text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: block;
+  font-size: 13px;
+  line-height: 23px;
+  margin: 0;
+  padding: 0;
+  color: V.$gray-700;
+}
+</style>
