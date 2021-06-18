acb4d128da3dcb8af3b6f1ef66c4c6fe8fb8f8c3
Adzhar Amrullah <adzharamrullah@gmail.com>
2021-06-19 01:31:11 +0700

    setup storybook jds button

--- src/components/JdsButton/Button.stories.js
+++ src/components/JdsButton/Button.stories.js
@@ -1,26 +1,87 @@
 // Button.stories.js
 
 import JdsButton from './Button.vue';
+import storybookMixin from '../../utils/storybook'
 
 export default {
   component: JdsButton,
   title: 'Components/Button',
   argTypes: {
-    placeholder: { control: 'text' },
+    responsiveness: 
+    {
+      description: 'The class fixed size width the button', 
+      control: {
+        type: 'inline-radio',
+        options: ['none', 'fix-3','fix-6','fix-9','fix-12','full-width']
+      } 
+    },
+    variant: 
+    { 
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary','tertiary','tertiary-paddingless','danger']
+      } 
+    },
   },
 };
 
 //👇 We create a “template” of how args map to rendering
-const Template = (args, { argTypes }) => ({
-  components: { JdsButton },
-  props: Object.keys(argTypes),
-  // Storybook provides all the args in a $props variable.
-  // Each arg is also available as their own name.
-  template: '<jds-button @click="onClick" v-bind="$props" v-on="$props" />',
-});
+const Template = (args, context) => {
+  console.log(args)
+  return {
+    name: 'JdsButtonStories',
+    components: { JdsButton },
+    mixins: [storybookMixin(args, context)],
+    computed: {
+      responsiveness(){
+        const { responsiveness } = args
+        return responsiveness !== 'none' ? responsiveness : ''
+      }
+    },
+    // Storybook provides all the args in a $props variable.
+    // Each arg is also available as their own name.
+    template: 
+    `<jds-button 
+      :class="responsiveness" 
+      v-bind="$props"
+      v-on="events"
+    />`,
+  }
+};
 
 //👇 Each story then reuses that template
 export const Default = Template.bind({});
 Default.args = {
-  placeholder: "Placeholder",
+  responsiveness: "none",
+  label: 'Button',
+  variant: 'primary'
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  ...Default.args
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  ...Default.args,
+  variant: 'secondary'
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  ...Default.args,
+  variant: 'tertiary'
+};
+
+export const TertiaryPaddingless = Template.bind({});
+TertiaryPaddingless.args = {
+  ...Default.args,
+  variant: 'tertiary-paddingless'
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  ...Default.args,
+  variant: 'danger'
 };
