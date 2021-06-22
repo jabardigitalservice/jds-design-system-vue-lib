f4b0bb2013ff8841846c08b7200256b1f6fe1a61
Adzhar Amrullah <adzharamrullah@gmail.com>
2021-06-22 11:40:48 +0700

    refactor scripts

--- scripts/build-esm.js
+++ scripts/build-esm.js
@@ -3,10 +3,4 @@ const shell = require('shelljs')
 const args = process.argv.slice(2)
 const watch = args.includes('--watch')
 shell.rm('-rf', 'publish/esm')
-shell.exec(`
-  npx cross-env NODE_ENV=build_esm babel src \
-  --config-file ./babel.lib.config.js \
-  --out-dir publish/esm/ \
-  --copy-files \
-  ${watch ? '--watch' : ''}
-`)
+shell.exec(`npx cross-env NODE_ENV=build_esm babel src --config-file ./babel.lib.config.js --out-dir publish/esm/  --copy-files ${watch ? '--watch' : ''}`)
