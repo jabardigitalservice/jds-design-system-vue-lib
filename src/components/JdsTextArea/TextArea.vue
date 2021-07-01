7985848a0d4f959f98dc521a58b401c16edcc07f
Ibwedagama <33661143+Ibwedagama@users.noreply.github.com>
2021-07-01 15:43:08 +0700

    JdsTextArea: Create component
    * create textarea component
    
    * fix onInput method; remove console statement
    
    * fix binding value props on textarea component
    
    * create textarea stories
    
    * remove placeholder attribute on textarea
    
    * add space on textarea stories
    
    * change container display to inline-block
    
    * export textarea component
    
    * JdsTextArea: rework css class naming using BEM convention
    
    * JdsTextArea: change class name on textarea template
    
    Co-authored-by: Adrian <adrian.padmodihardjo@gmail.com>

---
+++ src/components/JdsTextArea/TextArea.vue
@@ -0,0 +1,147 @@
+<template>
+  <div
+    :class="{
+      'jds-text-area font-sans-1': true,
+      'jds-text-area--empty': isEmpty,
+      'jds-text-area--error': showErrorMsg,
+      'jds-text-area--focused': isFocused,
+      'jds-text-area--hovered': isHovered,
+    }"
+  >
+    <jds-form-control-label v-if="showLabel">
+      {{ label }}
+    </jds-form-control-label>
+    <jds-form-control-helper-text v-if="showHelperText">
+      {{ helperText }}
+    </jds-form-control-helper-text>
+    <div
+      class="jds-text-area__input-wrapper"
+      @mouseenter="isHovered = true"
+      @mouseleave="isHovered = false"
+    >
+      <textarea
+        rows="7"
+        v-bind="inputAttributes"
+        :value="mValue"
+        @input="onInput"
+        @focus="isFocused = true"
+        @blur="isFocused = false"
+      />
+    </div>
+    <jds-form-control-error-message v-if="showErrorMsg">
+      {{ errorMessage }}
+    </jds-form-control-error-message>
+  </div>
+</template>
+
+<script>
+import {
+  JdsFormControlLabel,
+  JdsFormControlErrorMessage,
+  JdsFormControlHelperText,
+} from '../JdsFormControl'
+import localCopy from '../../mixins/local-copy'
+
+function isStringDefined(val) {
+  return typeof val === 'string' && !!val.length
+}
+
+export default {
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  mixins: [localCopy('value', 'mValue')],
+  components: {
+    JdsFormControlLabel,
+    JdsFormControlErrorMessage,
+    JdsFormControlHelperText,
+  },
+  name: 'jds-text-area',
+  props: {
+    /**
+     * Bound model.
+     * @name value
+     * @model
+     */
+    value: {
+      type: String,
+    },
+    /**
+     * Text area name attribute.
+     */
+    name: {
+      type: String,
+    },
+    /**
+     * Textarea placeholder attribute.
+     */
+    placeholder: {
+      type: String,
+    },
+    /**
+     * Textarea label.
+     */
+    label: {
+      type: String,
+    },
+    /**
+     * Textarea Hint.
+     */
+    helperText: {
+      type: String,
+    },
+    /**
+     * Display error message.
+     */
+    errorMessage: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      isFocused: false,
+      isHovered: false,
+      mValue: undefined,
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.mValue = e.target.value
+      this.emitInput(e.target.value)
+    },
+    emitInput(value) {
+      /**
+       * Emitted on input
+       * @param {string} value - updated bound model
+       */
+      this.$emit('input', value)
+    },
+  },
+  computed: {
+    inputAttributes() {
+      const attrs = ['name', 'placeholder']
+      return attrs.reduce((obj, key) => {
+        obj[key] = this[key]
+        return obj
+      }, {})
+    },
+    showLabel() {
+      return isStringDefined(this.label)
+    },
+    showHelperText() {
+      return isStringDefined(this.helperText)
+    },
+    showErrorMsg() {
+      return isStringDefined(this.errorMessage)
+    },
+    isEmpty() {
+      return !isStringDefined(this.mValue)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@use "./TextArea.scss";
+</style>
