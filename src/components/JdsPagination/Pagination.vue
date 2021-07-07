1a3da3abd96dd5900f87280d07274476da969449
Ibwedagama <wardana.wedagama@gmail.com>
2021-07-07 16:35:34 +0700

    create basic structure for pagination component

---
+++ src/components/JdsPagination/Pagination.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="jds-pagination font-sans-1">
+
+  <!-- left section -->
+  <div>
+
+  </div>
+
+  <!-- right section -->
+  <div class="jds-pagination__wrapper">
+    <div class="jds-pagination__navigation">
+      <!-- Left chevron -->
+      <button>
+        <
+      </button>
+    </div>
+
+    <div class="jds-pagination__page-control">
+      <!-- page section + dropdown will be here -->
+      Halaman
+      <select name="" id="">
+        <option value="1">1</option>
+        <option value="1">2</option>
+        <option value="1">3</option>
+      </select>
+      dari 10
+    </div>
+
+    <div class="jds-pagination__navigation">
+      <!-- Right section -->
+       <button>
+        >
+      </button>
+    </div>
+    
+  </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@use "./Pagination.scss";
+</style>
