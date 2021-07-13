10249ee6c0fb9a583661e55c4e768fac7ee52727
Ibwedagama <wardana.wedagama@gmail.com>
2021-07-13 12:03:41 +0700

    create basic structure and style for pagination component

--- src/components/JdsPagination/Pagination.vue
+++ src/components/JdsPagination/Pagination.vue
@@ -1,47 +1,49 @@
 <template>
-<div class="jds-pagination font-sans-1">
+  <div class="jds-pagination font-sans-1">
+    <div class="jds-pagination__wrapper">
+      <!-- left section -->
+      <div class="jds-pagination__page-control--left">
+        <div class="jds-pagination__page-control__select">
+          <span>
+            Tampilkan
+          </span>
+          <select name="" id="">
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+          <span>
+            item
+          </span>
+          <span>dari total <strong>100</strong></span>
+        </div>
+      </div>
 
-  <!-- left section -->
-  <div>
-
+      <!-- rigth section -->
+      <div class="jds-pagination__page-control--right">
+        <button class="jds-pagination__navigation-button">
+          <
+        </button>
+        <div class="jds-pagination__page-control__select">
+          Halaman
+          <select name="" id="">
+            <option value="1">1</option>
+            <option value="1">2</option>
+            <option value="1">3</option>
+          </select>
+          dari 10
+        </div>
+        <button class="jds-pagination__navigation-button">
+          >
+        </button>
+      </div>
+    </div>
   </div>
-
-  <!-- right section -->
-  <div class="jds-pagination__wrapper">
-    <div class="jds-pagination__navigation">
-      <!-- Left chevron -->
-      <button>
-        <
-      </button>
-    </div>
-
-    <div class="jds-pagination__page-control">
-      <!-- page section + dropdown will be here -->
-      Halaman
-      <select name="" id="">
-        <option value="1">1</option>
-        <option value="1">2</option>
-        <option value="1">3</option>
-      </select>
-      dari 10
-    </div>
-
-    <div class="jds-pagination__navigation">
-      <!-- Right section -->
-       <button>
-        >
-      </button>
-    </div>
-    
-  </div>
-</div>
-  
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
