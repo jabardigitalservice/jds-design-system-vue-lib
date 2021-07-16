9cf41bc1147a8bfeb9f79b14ac9043cc8d5528e4
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-16 11:00:30 +0700

    Revert "adjust JdsSelect to support filterable and option header"
    This reverts commit fa50df890a7b36a08887c6b0cb693dc55b2aec94.

--- src/components/JdsSelect/Select.vue
+++ src/components/JdsSelect/Select.vue
@@ -1,22 +1,17 @@
 <template>
   <jds-popover
     v-clickaway="onClickaway"
-    :class="{
-      'jds-select': true,
-      'jds-select--filterable': filterable,
-      'jds-select--has-header': !!optionsHeader,
-      'jds-select--opened': isDropdownOpen,
-      'jds-select--error': showErrorMsg & !isDropdownOpen
-    }"
     :value="isDropdownOpen" 
     :options="popperOptions"
-    @click.native.capture="onClickCaptured"
     @input="toggleDropdown"
   >
     <template #activator>
-      <div>
+      <div :class="{
+        'jds-select font-sans-1': true,
+        'jds-select--opened': isDropdownOpen,
+      }">
         <jds-input-text
-          ref="selectInputText"
+          ref="inputText"
           v-model="inputElementValue"
           readonly
           :label="label"
@@ -26,51 +21,31 @@
             'jds-input-text--error': showErrorMsg
           }"
           @click.native="onInputClicked"
-         >
+        >
           <template #suffix-icon>
             <jds-icon
               class="jds-select__trigger-icon"
               name="chevron-down"
               size="1em"
+              @click="onTriggerClicked"
             />
           </template>
         </jds-input-text>
         <jds-form-control-error-message
-          class="jds-select__error-message"
-        >
+          v-if="showErrorMsg && !isDropdownOpen"
+          class="jds-select__error-message">
           {{ errorMessage }}
         </jds-form-control-error-message>
       </div>
     </template>
-    <jds-popover-dropdown class="jds-select__dropdown">
-      <div class="jds-select__header">
-        <strong>{{ optionsHeader }}</strong>
-      </div>
-      <div class="jds-select__filter">
-        <jds-input-text
-          :value="mFilter"
-          ref="filterInputText"
-          class="jds-select__filter__input-text"
-          @input="onFilterInput"
-          @keydown.native.capture.passive="onKeydownFilterInput"
-        >
-          <template #prefix-icon>
-            <jds-icon
-              name="magnifier"
-              size="14px"
-              fill="#BDBDBD"
-            />
-          </template>
-        </jds-input-text>
-      </div>
+    <jds-popover-dropdown>
       <ul
         class="jds-select__options"
         ref="optionList"
       >
         <li
-          v-for="(opt, index) in availableOptions"
+          v-for="(opt, index) in mOptions"
           :key="index"
-          :data-index="index"
           ref="optionItemsEl"
           tabindex="0"
           :class="{
@@ -78,7 +53,6 @@
             'jds-select__option--selected': isOptionSelected(opt)
           }"
           @click="onClickOptionItem(opt)"
-          @keydown.stop.enter="onKeydownEnterOptionItem(opt)"
         >
           <span class="jds-select__option__text">
             {{ getOptionLabel(opt, labelKey) }}
@@ -128,7 +102,8 @@ export default {
   },
   mixins: [
     localCopy('value', 'mValue'),
-    localCopy('filter', 'mFilter'),
+    localCopy('filterValue', 'mFilterValue'),
+    localCopy('options', 'mOptions'),
   ],
   props: {
     /**
@@ -174,31 +149,6 @@ export default {
     },
 
     /**
-     * Set options as filterable by typing.
-     * Options will be filtered based on its label.
-     */
-    filterable: {
-      type: Boolean
-    },
-    /**
-     * One of `start,contain`.
-     * <br>
-     * start: filter using `String#startsWith`
-     * <br>
-     * contain: filter using `String#includes`
-     */
-    filterType: {
-      type: String,
-      default: 'start',
-    },
-    /**
-     * Initial filter value
-     */
-    filter: {
-      type: String
-    },
-
-    /**
      * Select label.
      */
     label: {
@@ -222,13 +172,6 @@ export default {
     errorMessage: {
       type: String,
     },
-
-    /**
-     * Set header text on options dropdown
-     */
-    optionsHeader: {
-      type: String
-    }
   },
   data () {
     return {
@@ -238,12 +181,10 @@ export default {
       },
       isDropdownOpen: false,
       mValue: undefined,
-      mFilter: undefined,
+      mFilterValue: undefined,
       inputElementValue: undefined,
 
-      // a hint that user is still focusing within
-      // JdsSelect, whether element is focusable or not
-      capturedClickedElement: null
+      mOptions: [],
     }
   },
   computed: {
@@ -257,31 +198,8 @@ export default {
       return isStringDefined(this.errorMessage)
     },
     currentOptionIndex () {
-      return this.availableOptions.findIndex((opt) => {
+      return this.mOptions.findIndex((opt) => {
         return getOptionValue(opt, this.valueKey) === this.mValue
-      })
-    },
-    filterMatcher () {
-      if (this.filterType === 'start') {
-        return (actual, expected) => actual
-          .toLowerCase()
-          .startsWith(expected.toLowerCase())
-      }
-      return (actual, expected) => actual
-        .toLowerCase()
-        .includes(expected.toLowerCase())
-    },
-    availableOptions () {
-      const canFilter = this.filterable
-        && typeof this.mFilter === 'string'
-        && this.mFilter.length
-      if (!canFilter) {
-        return this.options
-      }
-      const expected = this.mFilter.toLowerCase()
-      return this.options.filter((opt) => {
-        const actual = `${getOptionLabel(opt, this.labelKey)}`.toLowerCase()
-        return this.filterMatcher(actual, expected)
       })
     }
   },
@@ -296,17 +214,20 @@ export default {
     getOptionValue,
     findMatchedOption,
     findMatchedOptionByLabel (label) {
-      return this.availableOptions.find((opt) => {
+      return this.mOptions.find((opt) => {
         return getOptionLabel(opt, this.labelKey) === label
       })
     },
     listenToKeydownEvent (e) {
       const activeEl = document.activeElement
-      const isWithinThisPopover = !!this.capturedClickedElement
-        || this.$el?.contains?.(activeEl)
+      if (!activeEl) {
+        return
+      }
+      const isWithinThisPopover = this.$el?.contains?.(activeEl)
       if (!isWithinThisPopover) {
         return
       }
+
       this.handleKeyboardNavigation(e)
     },
 
@@ -332,103 +253,63 @@ export default {
       }
     },
     openDropdown () {
-      if (this.filterable) {
-        // TODO: find how to move focus from clicked element
-        // to another
-        setTimeout(() => {
-          this.focusOnFilterInput()
-        }, 50)
-      } else {
-        this.focusOnSelectInput()
-      }
       this.isDropdownOpen = true
     },
     closeDropdown ({ retainFocus = false } = {}) {
       this.isDropdownOpen = false
-      this.capturedClickedElement = null
       this.$nextTick(() => {
         if (retainFocus) {
-          this.focusOnSelectInput()
+          this.$refs.inputText.forceFocus()
         }
       })
     },
     // END: DROPDOWN STATE
 
     // START: NAVIGATION
-    getFocusableElements () {
-      const focusables = [
-        ...this.$refs.optionItemsEl
-      ]
-      if (this.filterable) {
-        focusables.unshift(this.$refs.filterInputText?.$refs?.inputEl)
-      }
-      return focusables
-    },
-    getCurrentFocusedElementIndex () {
-      if (!(document?.hasFocus())) {
-        return -1
-      }
-      const focusables = this.getFocusableElements()
-      return focusables.findIndex((el) => {
-        return document.activeElement === el
-      })
-    },
-    moveFocus (focusableIndex) {
-      const focusables = this.getFocusableElements()
-      if (focusableIndex >= 0 && focusableIndex < focusables.length) {
-        focusables[focusableIndex]?.focus()
-      }
-    },
-    focusOnSelectInput () {
-      this.$refs.selectInputText?.$refs?.inputEl?.focus?.()
-    },
-    focusOnFilterInput () {
-      this.$refs.filterInputText?.$refs?.inputEl?.focus?.()
-    },
     handleKeyboardNavigation(e) {
-      const isEnter = keyCode.isEnter(e)
-      const isTab = keyCode.isTab(e)
-      const isEscape = keyCode.isEscape(e)
-      const isGoingUp = keyCode.isArrow('Up', e)
-      const isGoingDown = keyCode.isArrow('Down', e)
+      if (keyCode.isArrow('Up', e) || keyCode.isArrow('Left', e)) {
+        this.navigateToPrevOption()
+      } else if (keyCode.isArrow('Down', e) || keyCode.isArrow('Right', e)) {
+        this.navigateToNextOption()
+      }
 
-      // when dropdown is not open
-      // arrow navigation will select option item
       if (!this.isDropdownOpen) {
-        if (isEnter) {
+        if (keyCode.isEnter(e)) {
           e.preventDefault()
           this.openDropdown()
-        } else if (isGoingUp) {
-          this.selectPreviousOption()
-        } else if (isGoingDown) {
-          this.selectNextOption()
         }
         return
       }
 
-      // when dropdown is open,
-      // navigation will move without selecting option item.
-      let currentFocusedIndex = this.getCurrentFocusedElementIndex()
-      
-      const shouldClose = this.capturedClickedElement
-        && currentFocusedIndex < 0
-        && (isTab || isEscape)
-
-      const forwardFocusToFilterInput = this.filterable
-          && this.capturedClickedElement
-          && currentFocusedIndex < 0
-          && (isEnter || isGoingUp || isGoingDown)
-
+      const shouldClose = keyCode.isTab(e)
+        || keyCode.isEscape(e)
+        || keyCode.isEnter(e)
       if (shouldClose) {
+        // prevent focus from moving to next focusable element,
+        // retain focus on input text element
         e.preventDefault()
         this.closeDropdown({ retainFocus: true })
-      } else if (forwardFocusToFilterInput) {
-        this.focusOnFilterInput()
-      } else if (isGoingUp) {
-        this.moveFocus(currentFocusedIndex - 1)
-      } else if (isGoingDown) {
-        this.moveFocus(currentFocusedIndex + 1)
       }
+    },
+    navigateToPrevOption () {
+      let i
+      if (this.currentOptionIndex <= 0) {
+        i = 0
+      } else {
+        i = this.currentOptionIndex - 1
+      }
+      const opt = this.mOptions[i]
+      this.changeSelectedOption(opt)
+      this.$refs.optionItemsEl[i].focus()
+    },
+    navigateToNextOption () {
+      if (this.currentOptionIndex === this.mOptions.length - 1) {
+        return
+      }
+      const i = this.currentOptionIndex + 1
+      const opt = this.mOptions[i]
+      this.changeSelectedOption(opt)
+      this.$refs.optionItemsEl[i].focus()
     },
     // END: NAVIGATION
 
@@ -447,27 +328,12 @@ export default {
       this.inputElementValue = getOptionLabel(option, this.labelKey)
       this.emitChange(this.mValue)
     },
-    selectPreviousOption() {
-      const i = Math.max(this.currentOptionIndex - 1, 0)
-      const opt = this.availableOptions[i]
-      this.changeSelectedOption(opt)
-    },
-    selectNextOption() {
-      const i = Math.min(
-        this.currentOptionIndex + 1,
-        this.availableOptions.length - 1
-      )
-      const opt = this.availableOptions[i]
-      this.changeSelectedOption(opt)
-    },
     // END: OPTION STATE
 
-    onClickCaptured (e) {
-      // only capture when dropdown is open
-      if (this.isDropdownOpen) {
-        this.capturedClickedElement = e.target
-      }
+    onTriggerClicked () {
+      this.$refs.inputText.forceFocus()
     },
+
     onInputClicked () {
       this.toggleDropdown()
     },
@@ -486,20 +352,6 @@ export default {
         this.changeSelectedOption(option)
       }
       if (this.autoClose) {
-        this.closeDropdown({ retainFocus: true })
-      }
-    },
-    onKeydownEnterOptionItem(option) {
-      this.changeSelectedOption(option)
-      this.closeDropdown({ retainFocus: true })
-    },
-    onFilterInput(str) {
-      this.mFilter = typeof str === 'string' && str.length
-        ? str
-        : null
-    },
-    onKeydownFilterInput (e) {
-      if (keyCode.isEscape(e)) {
         this.closeDropdown({ retainFocus: true })
       }
     },
