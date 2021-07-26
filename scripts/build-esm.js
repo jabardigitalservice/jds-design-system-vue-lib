d8d46a60c671fb1c96871abfc0c98c2e6ad7881b
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-27 06:31:29 +0700

    copy raw scss files to /publish

--- scripts/build-esm.js
+++ scripts/build-esm.js
@@ -2,5 +2,6 @@ const shell = require('shelljs')
 
 const args = process.argv.slice(2)
 const watch = args.includes('--watch')
-shell.rm('-rf', 'publish/esm')
+shell.rm('-rf', 'publish/esm', 'publish/styles')
+shell.cp('-r', 'styles/', 'publish/styles/')
 shell.exec(`npx cross-env NODE_ENV=build_esm babel src --config-file ./babel.lib.config.js --out-dir publish/esm/  --copy-files ${watch ? '--watch' : ''}`)
