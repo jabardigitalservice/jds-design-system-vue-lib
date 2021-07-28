2c7c09b9631a366cba41718016d47a0b838dca4f
Ibwedagama <wardana.wedagama@gmail.com>
2021-07-28 14:20:12 +0700

    chore(data-table): create sort functionality mixin

--- src/components/JdsDataTable/mixins/sort.js
+++ src/components/JdsDataTable/mixins/sort.js
@@ -1,10 +1,11 @@
 const sort = {
   /**
    * Populate sorting object when the component mounted.
+   * return an Array of Objects.
    */
   mounted() {
-    if (Array.isArray(this.headers) && this.headers.length) {
-      return this.sortObject = this.headers.map((header) => {
+    if (Array.isArray(this.tableHeaders) && this.tableHeaders.length) {
+      return this.sortObject = this.tableHeaders.map((header) => {
         return { id: header.key, toggle: false, sortBy: 'asc' }
       })
     }
@@ -13,24 +14,24 @@ const sort = {
 
   methods: {
     /**
-     * Check if the header is sortable
+     * Check if the header is sortable.
+     * return a Boolean.
      */
-    isSortable(id) {
-      if (Array.isArray(this.sort)) {
-        return this.sort.includes(id)
-      }
-      return false
+    isSortable(header) {
+      return header.sortable ?? false
     },
 
     /**
-     * 
+     * Check if the header with specific id currently being sorted.
+     * return a Boolean.
      */
     isSorted(id) {
       return !!this.sortObject.find((obj) => (obj.id === id ? obj.toggle : false))
     },
 
     /**
-     * 
+     * Mutate `sortObject` object when sort button is clicked.
+     * Emit an event with an object {`someKey`: "asc" | "desc"}
      */
     onSort(id) {
       const newObject = this.sortObject.map((obj) =>
@@ -50,27 +51,25 @@ const sort = {
       this.sortObject = newObject
 
       const object = this.sortObject.find((obj) => obj.id === id)
+      object.sortBy === 'asc' ? this.sortByAscendingOrder(id) : this.sortByDescendingOrder(id)
 
-      if (object.sortBy === 'asc') {
-        this.sortByAscendingOrder(id)
-      } else {
-        this.sortByDescendingOrder(id)
-      }
+      this.$emit('change:sort', {[object.id]: object.sortBy})
     },
 
     /**
-     * 
+     * sort local data with ascending order.
      */
     sortByAscendingOrder(property) {
       this.mItems = this.items.sort((a, b) => (a[property] > b[property]) - (a[property] < b[property]))
     },
 
     /**
-     * 
+     * sort local data with descending order.
      */
     sortByDescendingOrder(property) {
       this.mItems = this.items.sort((a, b) => (b[property] > a[property]) - (b[property] < a[property]))
     },
+
   }
 }
 
